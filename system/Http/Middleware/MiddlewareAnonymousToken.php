8b3587940f97c3f38e2d9aa3b1da512579b78274
myusername <myusername@myemaildomain.com>
2018-10-12 22:32:33 +0400

    Kernel

---
+++ system/Http/Middleware/MiddlewareAnonymousToken.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 12.10.2018
+ * Time: 21:29
+ */
+
+namespace Http\Middleware;
+
+use Configs\Config;
+use Http\Cookie;
+use Http\Request\ServerRequest;
+use Http\Session\SessionRedis;
+use System\Auth\JWTokenManager;
+use System\Kernel\TypesApp\AbstractApplication;
+use System\Registry;
+
+class MiddlewareAnonymousToken
+{
+	/**
+	 * @param ServerRequest $request
+	 * @param RequestHandler $handler
+	 * @return \Http\Response\Response
+	 * @throws \Exception\FileException
+	 * @throws \Exception
+	 */
+	public function process(ServerRequest $request, RequestHandler $handler)
+	{
+		$cookie     = Cookie::create()->get('JWT');
+		$currentApp = Registry::get(Registry::APP);
+
+		if ($currentApp->getApplicationType() === AbstractApplication::APP_TYPE['Auth']) {
+			return $handler->handle($request, $handler);
+		}
+
+		if (empty($cookie)) {
+			$this->createAnonymousJWToken();
+		}
+
+		return $handler->handle($request, $handler);
+	}
+
+	/**
+	 * @throws \Exception\FileException
+	 * @throws \Exception
+	 */
+	private function createAnonymousJWToken()
+	{
+		$expires = \time() + (int) Config::get('common', 'timeAnonJWToken');
+
+		$dataToSave = [
+			'uniqueId' => Util::generateCookieToken(20),
+			'ip'       => ServerRequest::create()->getUserIp(),
+			'userId'   => 0,
+			'email'    => '',
+			'role'     => 0,
+			'login'    => '',
+			'created'  => \microtime(true),
+			'iat'      => \time(),
+			'exp'      => \time() + $expires,
+		];
+
+		$JWTToken = JWTokenManager::create()->setPayload($dataToSave)->createToken();
+		$result   = SessionRedis::create()->set($JWTToken->getPartToken(JWTokenManager::SIGN_TOKEN), \json_encode([]), $expires);
+
+		if ($result) {
+			Cookie::create()->set('JWT', $JWTToken->getToken(), '', $expires);
+		}
+	}
+}
